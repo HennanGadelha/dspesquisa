2c30ba794ef37029247b2dfc7911fdb5e241a8af
Hennan Gadelha <hennanhennan@gmail.com>
2020-09-14 23:29:28 -0300

    implementando repositories

---
+++ backend/src/main/java/com/devsuperior/dspesquisa/repositories/GameRepository.java
@@ -0,0 +1,12 @@
+package com.devsuperior.dspesquisa.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.dspesquisa.entities.Genre;
+
+public interface GameRepository extends JpaRepository<Genre, Long> {
+
+	
+	
+	
+}
---
+++ backend/src/main/java/com/devsuperior/dspesquisa/repositories/RecordRepository2.java
@@ -0,0 +1,12 @@
+package com.devsuperior.dspesquisa.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.dspesquisa.entities.Record;
+
+public interface RecordRepository2 extends JpaRepository<Record, Long> {
+
+	
+	
+	
+}
