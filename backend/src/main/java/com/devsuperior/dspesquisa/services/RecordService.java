a523b627279eabd31fc9898cf26467ab401bfc8d
Hennan Gadelha <hennanhennan@gmail.com>
2020-09-15 22:41:33 -0300

    POST records

---
+++ backend/src/main/java/com/devsuperior/dspesquisa/services/RecordService.java
@@ -0,0 +1,50 @@
+package com.devsuperior.dspesquisa.services;
+
+import java.time.Instant;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dspesquisa.dto.RecordDTO;
+import com.devsuperior.dspesquisa.dto.RecordInsertDTO;
+import com.devsuperior.dspesquisa.entities.Game;
+import com.devsuperior.dspesquisa.entities.Record;
+import com.devsuperior.dspesquisa.repositories.GameRepository;
+import com.devsuperior.dspesquisa.repositories.RecordRepository;
+
+@Service
+public class RecordService {
+
+	@Autowired
+	private RecordRepository repository;
+	
+	@Autowired
+	private GameRepository gameRepository;
+	
+	//Transaction é forma de garantir que toda
+	//parte de banco de dados seja realizado no service
+	
+	//  o readOnly é importante para evitar o lock do banco de dados
+	
+	@Transactional
+	public RecordDTO insert(RecordInsertDTO recordDTO) {
+		
+		Record record = new Record();
+		
+		record.setName(recordDTO.getName());
+		record.setAge(recordDTO.getAge());
+		record.setMoment(Instant.now());
+		
+		Game game = gameRepository.getOne(recordDTO.getGameId());
+		record.setGame(game);
+		
+		record = repository.save(record);
+		
+		return new RecordDTO(record);
+		
+	}
+	
+	
+	
+}
