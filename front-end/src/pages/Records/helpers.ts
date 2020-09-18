eac230d74de5015ae88b067c3005e6a4539caeb2
Hennan Gadelha <hennanhennan@gmail.com>
2020-09-18 01:27:53 -0300

    criando pagina de listagem dos registros

---
+++ front-end/src/pages/Records/helpers.ts
@@ -0,0 +1,7 @@
+import moment from 'moment';
+
+export const formatDate = (date: string) => {
+
+    return moment(date).format('DD/MM/YYYY HH:mm');
+
+}
--- front-end/src/pages/Records/index.tsx
+++ front-end/src/pages/Records/index.tsx
@@ -1,8 +1,90 @@
-import React from 'react';
+import Axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import './styles.css';
+import { RecordsResponse } from './types';
+import { formatDate} from './helpers';
+import Pagination from './Pagination';
+import { Link } from 'react-router-dom'
+
+
+const BASE_URL = 'http://localhost:8080'
 
 const Records = () =>{
+    //criando estado interno para o componente funcionar
+    const [ recordsResponse, setRecordsResponse] = useState<RecordsResponse>();
+    const [activePage, setActivePage] = useState(0);
 
-    return <h1>records</h1>
+    console.log(recordsResponse)
+    //use effect -> ciclo de vida de um componente
+    useEffect(()=>{
+
+        //console.log("compenente iniciado")
+
+        Axios.get(`${BASE_URL}/records?linesPerPage=12&page=${activePage}`)
+        .then(response => setRecordsResponse(response.data))
+
+
+    }, [activePage])
+
+    const handlePageChange =(index: number) => {
+         setActivePage(index)
+    }
+
+
+    return (
+
+        <div className="page-container">
+  
+            <div className="filters-container records-actions">
+
+                <Link to="/charts">
+
+                    <button className="action-filters">Ver graficos</button>
+
+                </Link>
+
+            </div>
+          <table className="records-table" cellPadding="0" cellSpacing="0">
+  
+              <thead>
+                  <tr>
+                      <th>INSTANTE</th>
+                      <th>NOME</th>
+                      <th>IDADE</th>
+                      <th>PLAFORMA</th>
+                      <th>GENERO</th>
+                      <th>TITULO DO GAME</th>
+                  </tr>
+              </thead>
+  
+              <tbody>
+
+                  {recordsResponse?.content.map(record => (
+
+                    <tr key={record.id}>
+                        <td>{formatDate(record.moment)}</td>
+                        <td>{record.name}</td>
+                        <td>{record.age}</td>
+                        <td className="text-secondary ">{record.gamePlataform}</td>
+                        <td>{record.genreName}</td>
+                        <td className="text-primary">{record.gameTitle}</td>
+                    </tr>
+
+                ))}
+
+  
+              </tbody>
+  
+          </table>
+
+       <Pagination  activePage={activePage}
+        goToPage={handlePageChange}
+       totalPages= {recordsResponse?.totalPages}/>
+
+      </div>
+  
+  )
+
 
 };
 
