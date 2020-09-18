5e05fdd6059a27dc2326145dbbc377e25a363ed6
Hennan Gadelha <hennanhennan@gmail.com>
2020-09-17 21:09:07 -0300

    adicionando componente header

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
+import Header from './components/header';
+
 
 function App() {
   return (
-    <h1>Hello sds</h1>
+
+    <div>
+      <Header />
+
+    </div>
+
   );
 }
 
 export default App;
+ 
---
+++ front-end/src/components/header/index.tsx
@@ -0,0 +1,22 @@
+import { ReactComponent } from '*.svg';
+import React from 'react';
+import './styles.css';
+import { ReactComponent as Logo} from '../../assets/logo.svg';
+
+const Header = () =>{
+
+    return <header className="main-header">
+
+    <Logo />
+    <div className="logo-text">
+
+        <span className="logo-text-1">Big Game</span>
+        <span className="logo-text-2"> Survey</span>
+
+
+    </div>
+
+    </header>
+}
+
+export default Header;
